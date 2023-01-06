f6c236294b01d2b922cdba6a205dc418b1bf1e01
elOdio <60266913+seoihgv@users.noreply.github.com>
2023-01-05 22:09:28 -0600

    Add gulp task to compile SASS file into CSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,21 @@
-//example function on how to use Gulp
-function tarea(done) {
-    console.log('mi primer tarea');
+// //example function on how to use Gulp
+// function tarea(done) {
+//     console.log('mi primer tarea');
 
-    done();
+//     done();
+// }
+
+// exports.tarea = tarea;
+
+const { src, dest } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+function css(done) {
+    //identificar el archivo SASS
+    //Compilarlo
+    //Almacenarlo en el disco duro
+    src('src/scss/app.scss').pipe(sass()).pipe(dest('build/css'));
+
+    done(); // callback que avisa a Gulp cuando llegamos al final
 }
 
-exports.tarea = tarea;
+exports.css = css;
