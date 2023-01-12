7b4899e2a3400ed283d9284d53e240436ec069c5
elOdio <60266913+seoihgv@users.noreply.github.com>
2023-01-11 18:37:43 -0600

    Add function to close the modal window of the gallery

--- build/js/app.js
+++ build/js/app.js
@@ -29,10 +29,30 @@ function mostrarImagen(id) {
                     <source srcset="build/img/grande/${id}.webp" type="image/webp">
                     <img loading="lazy" width="200" height="300" src="build/img/grande/${id}.jpg" alt="imagen galería"/>
                 `;
+
+    // Create the overlay with the image
     const overlay = document.createElement('DIV');
     overlay.appendChild(imagen);
     overlay.classList.add('overlay');
+    overlay.onclick = function () {
+        const body = document.querySelector('body');
+        body.classList.remove('fijar-body');
+        overlay.remove();
+    };
 
+    //Close the modal
+    const closeModal = document.createElement('P');
+    closeModal.textContent = 'X';
+    closeModal.classList.add('btn-cerrar');
+    closeModal.onclick = function () {
+        const body = document.querySelector('body');
+        body.classList.remove('fijar-body');
+        overlay.remove();
+    };
+    overlay.appendChild(closeModal);
+
+    // Add the overlay in the HTML document
     const body = document.querySelector('body');
     body.appendChild(overlay);
+    body.classList.add('fijar-body');
 }
