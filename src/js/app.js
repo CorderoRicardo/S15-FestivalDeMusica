337ccbd87a60e1d4373739198a46852b3c6fd90a
elOdio <60266913+seoihgv@users.noreply.github.com>
2023-01-28 13:29:57 -0600

    Add a CSS class to the thumbnail images in the gallery

--- src/js/app.js
+++ src/js/app.js
@@ -50,6 +50,7 @@ function crearGaleria() {
     const galeria = document.querySelector('.galeria-imagenes');
     for (let i = 1; i <= 12; i++) {
         const imagen = document.createElement('picture');
+        imagen.classList.add('small-img');
         imagen.innerHTML = `
                     <source srcset="build/img/thumb/${i}.avif" type="image/avif">
                     <source srcset="build/img/thumb/${i}.webp" type="image/webp">
